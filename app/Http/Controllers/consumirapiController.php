49b8c56ceac5b7cf3e44da70daa13430d3a35009
TadeuMiranda01 <tadeumiranda0003@gmail.com>
2022-11-21 12:22:49 -0300

    Primeiro

---
+++ app/Http/Controllers/consumirapiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http; 
+use Codenixsv\CoinGeckoApi\CoinGeckoClient;
+
+class consumirapiController extends Controller
+{
+    public function index($coin,$data = null)
+    {  
+
+        $client = new CoinGeckoClient();
+        if($data == true){
+
+            $result = $client->coins()->getHistory($coin, $data);
+            dd($result);
+        }else{
+            $data = date('d-m-Y');
+            $result = $client->coins()->getHistory($coin, $data);
+            dd($result);
+        }
+    }
+}
